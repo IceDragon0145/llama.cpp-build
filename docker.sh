b718a00cc79aff714255509dede201b88f06abc5
Boris Chen <boris200311@gmail.com>
2024-09-15 20:29:55 +0800

    Initial commit

--- docker.sh
+++ docker.sh
@@ -7,6 +7,6 @@ docker build \
     --no-cache \
     --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
     --build-arg CUDA_VERSION=$CUDA_VERSION \
-    "$GITHUB_WORKSPACE/Dockerfile"
+    .
 docker run -t llama-cpp-build --name llama-cpp-build-container
 docker cp llama-cpp-build-container:/build.tar.gz .
